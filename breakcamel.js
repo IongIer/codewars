88a4f7eef8cb148d1b3d25b774f63b1f1c978126
oni <mosionroatarevine@gmail.com>
2022-07-16 10:42:09 +0300

    Js same regex, positive lookahead correct

--- breakcamel.js
+++ breakcamel.js
@@ -1,5 +1,5 @@
 // Complete the solution so that the function will break up camel casing, using a space between words.
 
 function solution(string) {
-    return solution..split(/(?=[A-Z])/);
+    return string.split(/(?=[A-Z])/).join(" ");
 }
