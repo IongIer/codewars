f21522ff7ac40e46f496128665d0f03f37e0a38e
oni <mosionroatarevine@gmail.com>
2022-07-18 08:36:06 +0300

    Just .title() didn't work

---
+++ jaden.py
@@ -0,0 +1,7 @@
+# Your task is to convert strings to how they would be written by Jaden Smith.
+# The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+
+def to_jaden_case(string):
+    cased = [word.capitalize() for word in string.split(" ")]
+    return " ".join(cased)
