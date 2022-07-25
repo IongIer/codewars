8f08a5513b7a0b71986e942127d67a0a64baa07e
oni <mosionroatarevine@gmail.com>
2022-07-25 12:49:04 +0300

    More efficient than mine

---
+++ arraydif.py
@@ -0,0 +1,5 @@
+# better than my solution and important not to make the set inside the list comp as it does it every loop
+
+def array_diff(a, b):
+    set_b = set(b)
+    return [i for i in a if i not in set_b]
