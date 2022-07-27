c882e9e2b5d3585bfcf602e76e47e842d8094a44
oni <mosionroatarevine@gmail.com>
2022-07-27 10:36:57 +0300

    Stolen bitwise solution

--- oddint.py
+++ oddint.py
@@ -2,11 +2,19 @@
 
 # There will always be only one integer that appears an odd number of times.
 
-from collections import Counter
+#Bad solution
+# from collections import Counter
 
+
+# def find_it(seq):
+#     count = Counter(seq)
+#     for x in count:
+#         if count[x] % 2 != 0:
+#             return x
+from functools import reduce
+import operator
 
 def find_it(seq):
-    count = Counter(seq)
-    for x in count:
-        if count[x] % 2 != 0:
-            return x
+    return reduce(operator.xor , seq)
+
+print(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
