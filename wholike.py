1c47f3055937887f7e7ad8406f74a2f8d12b8297
oni <mosionroatarevine@gmail.com>
2022-08-03 11:59:41 +0300

    match statement is like a switch statement but seems more powerful

--- wholike.py
+++ wholike.py
@@ -24,3 +24,21 @@ def likes(names):
         return f"{names[0]}, {names[1]} and {names[2]} like this"
     else:
         return f"{names[0]}, {names[1]} and {total-2} others like this"
+
+
+#python 3.10 solution
+
+
+def likes_match(names):
+    match(len(names)):
+        case 0:
+            return 'no one likes this'
+        case 1:
+            return f'{names[0]} likes this'
+        case 2:
+            return f'{names[0]} and {names[1]} like this'
+        case 3:
+            return f'{names[0]}, {names[1]} and {names[2]} like this'
+        case _:
+            return f'{names[0]}, {names[1]} and {len(names)-2} others like this'
+
