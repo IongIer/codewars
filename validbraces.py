10fb6782e5a261e2f7511c8038665d08a12f3f76
oni <mosionroatarevine@gmail.com>
2022-09-02 17:05:33 +0300

    same solution as leetcode

---
+++ validbraces.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def valid_braces(s):
+        valid_start = "({["
+        pairs = {")": "(", "]": "[", "}": "{"}
+        if len(s) % 2 == 0:
+            stack = deque()
+            if s[0] not in valid_start:
+                return False
+            for i, v in enumerate(s):
+                if v in valid_start:
+                    stack.append(v)
+                else:
+                    try:
+                        rem = stack.pop()
+                        if rem != pairs[v]:
+                            return False
+                    except IndexError:
+                        return False
+            return not len(stack)
